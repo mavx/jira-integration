7be3205bda1c12015d5ca6ee29ae8b56d49af6fe
Au Yong Min Hao <mauyong@moneylion.com>
2019-06-04 11:58:46 +0800

    Blacken code

--- config.py
+++ config.py
@@ -1,16 +1,12 @@
 import json
 import os
 
-CONFIG_FILE = 'config.json'
+CONFIG_FILE = "config.json"
 
 
 def setup(email, token, server):
-    config = {
-        'email': email,
-        'token': token,
-        'server': server
-    }
-    with open(CONFIG_FILE, 'w') as o:
+    config = {"email": email, "token": token, "server": server}
+    with open(CONFIG_FILE, "w") as o:
         o.write(json.dumps(config, indent=2))
         print("Credentials saved to `{}`".format(CONFIG_FILE))
 
@@ -21,14 +17,14 @@ def read():
     email, token, server = None, None, None
     if os.path.isfile(CONFIG_FILE):
         print("Reading credentials from `{}`".format(CONFIG_FILE))
-        with open(CONFIG_FILE, 'r') as f:
+        with open(CONFIG_FILE, "r") as f:
             config = json.loads(f.read())
 
-        email = config.get('email')
-        token = config.get('token')
-        server = config.get('server')
+        email = config.get("email")
+        token = config.get("token")
+        server = config.get("server")
     return email, token, server
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(read())
